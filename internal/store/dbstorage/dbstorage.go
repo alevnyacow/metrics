86f37019afbed17d986e5d4d616b94c4f9df899a
alevny <alevnyacow@gmail.com>
2025-05-17 08:37:12 +0300

    added with retries

--- internal/store/dbstorage/dbstorage.go
+++ internal/store/dbstorage/dbstorage.go
@@ -6,48 +6,52 @@ import (
 
 	"github.com/alevnyacow/metrics/internal/retries"
 	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 	"github.com/rs/zerolog/log"
 )
 
 func InitDatabase(connectionString string, migrationsPath string) (db *sql.DB, closeConnection func()) {
-	database, err := sql.Open("postgres", connectionString)
+	err := retries.WithRetries(func() error {
+		migration, err := migrate.New(
+			fmt.Sprintf("file://%s", migrationsPath),
+			connectionString,
+		)
+
+		if err != nil {
+			log.Err(err).Msg("Error on migration initialization")
+			return err
+		}
+
+		if errOnUp := retries.WithRetries(migration.Up); errOnUp != nil {
+			log.Err(errOnUp).Msg("Error on applying migrations")
+			errOnDown := retries.WithRetries(migration.Down)
+			if errOnDown != nil {
+				log.Err(errOnDown).Msg("Error on dropping migrations")
+			}
+		}
+
+		database, err := sql.Open("postgres", connectionString)
+		if err != nil {
+			return err
+		}
+		db = database
+		closeConnection = func() {
+			if db != nil {
+				err := db.Close()
+				if err != nil {
+					log.Err(err).Msg("Error on closing database")
+				}
+			}
+		}
+		return nil
+	})
+
 	if err != nil {
 		panic(err)
 	}
-	db = database
-	driver, err := postgres.WithInstance(db, &postgres.Config{})
-	if err != nil {
-		log.Err(err).Msg("Error creating driver")
-	}
-	migration, err := migrate.NewWithDatabaseInstance(
-		fmt.Sprintf("file://%s", migrationsPath),
-		"postgres",
-		driver,
-	)
 
-	if err != nil {
-		log.Err(err).Msg("Error on migration initialization")
-	}
-
-	if errOnUp := retries.WithRetries(migration.Up); errOnUp != nil {
-		log.Err(errOnUp).Msg("Error on applying migrations")
-		errOnDown := retries.WithRetries(migration.Down)
-		if errOnDown != nil {
-			log.Err(errOnDown).Msg("Error on dropping migrations")
-		}
-	}
-
-	closeConnection = func() {
-		if db != nil {
-			err := db.Close()
-			if err != nil {
-				log.Err(err).Msg("Error on closing database")
-			}
-		}
-	}
 	return
 }
 
